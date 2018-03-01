0581bcfa9aa5a78a53d02461e17b4ae815a45ac6
atakanguney <atakanguney58@hotmail.com>
2018-03-01 23:39:20 +0300

    update script

--- hashcode2018_atakan.py
+++ hashcode2018_atakan.py
@@ -33,17 +33,16 @@ class Ride:
         self.latest = conf[5]
         self.distance = self.start.distance(self.end)
         
-    def findCar(self, vehicles, time):
+    def findCar(self, vehicles):
+        print('[DEBUG] findCar method')
         for i in vehicles:
-            if(vehicles[i].reachOnTime(self, time)[0]):
-                vehicles[i].available.append((self.early, self.latest,self.r_id))
-                break
+            vehicles[i].addToAvailable(self)
         
 class Vehicle:
     
     def __init__(self, row, col):
         self.location = Intersection(row, col)
-        self.available = []
+        self.available = [(0,0,Ride([0,0,0,0,-1,-1],-1)),(float('inf'),float('inf'),Ride([0,0,0,0,float('inf'),float('inf')],-2))]
         
     def __str__(self):
         ret_str = ''
@@ -52,26 +51,32 @@ class Vehicle:
         return ret_str
  
     def addToAvailable(self, newRide):
-        
-        for i in range(len(available)-1):
-            available, eps_new, epf_new = bool_put_ride_in_between(self.available[i], self.available[i+1], newRide)
+        print('[DEBUG] addToAvailable method')
+        for i in range(len(self.available)-1):
+            available, eps_new, epf_new = self.put_between(self.available[i], self.available[i+1], newRide)
             if(available):
-                self.available.insert((eps_new, epf_new,newRide.id),i)
+                self.available.insert((eps_new, epf_new,newRide),i)
                 break
             
-    def reachOnTime(self, ride, now_time):
-        car_to_start_dist = self.location.distance(ride.start)
+    def put_between(self, tuple1, tuple2, newRide):
+        print('[DEBUG] put between')
+        is_poss, _ , _ , _ = self.reachOnTime(tuple1[2].end, newRide, tuple1[1])
+        print(is_poss)
+        
+        
+    def reachOnTime(self, loc, ride, now_time):
+        print('[DEBUG] reach on time')
+        car_to_start_dist = loc.distance(ride.start)
         start_to_dest_dist = ride.distance
         total_distance = car_to_start_dist + start_to_dest_dist
         total_time = ride.latest - now_time
     
         if now_time>ride.latest:
             print('ERROR Latest finish is already past.')
-            return -1,-1,-1,-1
+            return False,-1,False,-1
         
         total_waiting_time = total_time - total_distance
         is_it_possible_ride = total_waiting_time >= 0
-        self.location = ride.end
         
         precise_waiting_time = (ride.early - now_time) - car_to_start_dist
         is_it_possible_precise_start = precise_waiting_time >= 0
@@ -94,8 +99,11 @@ if __name__ == '__main__':
     N = int(first_line[3])
     B = int(first_line[4])
     T = int(first_line[5])
-    
+    begin_ride = Ride([0,0,0,0,-1,-1],-1)
+    end_ride = Ride([0,0,0,0,float('inf'),float('inf')],-2)
     rides = {}
+    rides[-1] = begin_ride
+    rides[-2] = end_ride
     id = 0
     for ride in lines[1:]:
         temp = ride.split()
@@ -116,14 +124,13 @@ if __name__ == '__main__':
     
     myList = []
     for i in rides:
-        rides[i].findCar(vehicles,0)
+        rides[i].findCar(vehicles)
         myList.append((i, rides[i].distance))
         
     myList.sort(key=lambda tup: tup[1])
     
     for i in vehicles:
-        print('Location: ' + str(vehicles[i].location))
-        print(vehicles[i])
+        print(vehicles[i].available)
 
         
     
